6bb61ee3bd2ad3c3947126c06e63dd2b18fa4768
PRASENJIT BEHERA <prasenjitbehera2005@gmail.com>
2026-05-13 20:18:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+// MOBILE MENU
 
+const menuIcon =
+document.getElementById("menuIcon");
+
+const navMenu =
+document.getElementById("navMenu");
+
+menuIcon.addEventListener("click",()=>{
+
+  navMenu.classList.toggle("active");
+
+  const icon =
+  menuIcon.querySelector("i");
+
+  if(navMenu.classList.contains("active")){
+
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-xmark");
+
+  }else{
+
+    icon.classList.remove("fa-xmark");
+    icon.classList.add("fa-bars");
+  }
+
+});
+
+// CONTACT FORM
+
+const contactForm =
+document.getElementById("contactForm");
+
+contactForm.addEventListener("submit",(e)=>{
+
+  e.preventDefault();
+
+  alert(
+  "Thank You! Your message has been submitted."
+  );
+
+  contactForm.reset();
+
+});
